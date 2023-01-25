7876700d0b7a7c2c191efd47be5d05fcc3e84f56
helmics251 <117761635+helmics251@users.noreply.github.com>
2023-01-25 19:33:08 +0800

    Update serverQuestion1.c

--- serverQuestion1.c
+++ serverQuestion1.c
@@ -5,74 +5,69 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <unistd.h> // read(), write(), close()
+#include <unistd.h> 
 #include <arpa/inet.h>
-#define MAX 80
-#define PORT 8080
 #define SA struct sockaddr
 
-// Function designed between client and server.
-void func(int connfd)
-{
-        char buff[MAX];
-        int randomNum = (rand() % 900) + 100;
 
-         bzero(buff, sizeof(buff));
+void func(int connect_desc)
+{
+        char buff[80];
+        int randomNum = (rand() % 900) + 100;
+        bzero(buff, sizeof(buff));
         memset(buff, '\0', 256);
         sprintf(buff, "%d", randomNum);
-        write(connfd, buff, sizeof(buff));
+        write(connect_desc, buff, sizeof(buff));
 }
 
-// Driver function
+
 int main()
 {
-        int sockfd, connfd, len;
-        struct sockaddr_in servaddr, cli;
+        int socket_desc, connect_desc, length;
+        struct sockaddr_in server, cli;
 
-        // socket create and verification
-        sockfd = socket(AF_INET, SOCK_STREAM, 0);
-        if (sockfd == -1) {
+        
+        socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+        if (socket_desc == -1) {
                 printf("socket creation failed...\n");
                 exit(0);
         }
         else
                 printf("Socket successfully created..\n");
-        bzero(&servaddr, sizeof(servaddr));
+        bzero(&server, sizeof(server));
 
-        // assign IP, PORT
-        servaddr.sin_family = AF_INET;
-        servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-        servaddr.sin_port = htons(PORT);
+        
+        server.sin_family = AF_INET;
+        server.sin_addr.s_addr = htonl(INADDR_ANY);
+        server.sin_port = htons(8080);
 
-        // Binding newly created socket to given IP and verification
-        if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
+        
+        if ((bind(socket_desc, (SA*)&server, sizeof(server))) != 0) {
                 printf("socket bind failed...\n");
                 exit(0);
         }
         else
                 printf("Socket successfully binded..\n");
 
-        // Now server is ready to listen and verification
-        if ((listen(sockfd, 5)) != 0) {
+        
+        if ((listen(socket_desc, 5)) != 0) {
                 printf("Listen failed...\n");
                 exit(0);
         }
         else
                 printf("Server listening..\n");
-        len = sizeof(cli);
+        length = sizeof(cli);
 
-        // Accept the data packet from client and verification
-        connfd = accept(sockfd, (SA*)&cli, &len);
-        if (connfd < 0) {
+        
+        connect_desc = accept(socket_desc, (SA*)&cli, &length);
+        if (connect_desc < 0) {
                 printf("server accept failed...\n");
                 exit(0);
         }
         else
                 printf("server accept the client...\n");
 
-        // Function for chatting between client and server
-        func(connfd);
-
-        // After chatting close the socket
-        close(sockfd);
+        
+        func(connect_desc);
+        close(socket_desc);
 }
