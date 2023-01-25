98d4ad648f4b4a356b432b8dfe1b722705a58728
helmics251 <117761635+helmics251@users.noreply.github.com>
2023-01-25 19:51:21 +0800

    Create Question2Client.py

---
+++ Question2Client.py
@@ -0,0 +1,16 @@
+import socket
+
+def main():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('192.168.191.128', 1234))
+
+    fahren = float(input("Enter temperature in Fahrenheit: "))
+    client_socket.send(str(fahren).encode())
+
+    celsius = client_socket.recv(1024).decode()
+    print(f"Temperature in Celsius: {cels}")
+
+    client_socket.close()
+
+if __name__ == '__main__':
+    main()
